afcff8ccd394d1d75bf42c620071913286ce40ca
elmira1385 <elmirashokriyan64@gmail.com>
2026-07-17 23:33:27 +0430

    start

---
+++ src/api/axios.ts
@@ -0,0 +1,19 @@
+
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://your-api.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
