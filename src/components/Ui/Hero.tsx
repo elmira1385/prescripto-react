2fa6550c200a1dfbf36ebd38ee4caed439643193
elmira1385 <elmirashokriyan64@gmail.com>
2026-07-23 00:47:30 +0430

    hero was added

---
+++ src/components/Ui/Hero.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import groupHero from "../../images/group-hero.png";
+import hero from "../../images/hero.png";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col gap-8 justify-center  rounded-md p-4 pb-0 mt-4 bg-primary">
+      <div className="flex flex-col gap-6 justify-center items-center">
+        <p className="text-3xl pt-4 text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
+          Book Appointment
+          <br /> With Trusted Doctors
+        </p>
+        <img width={120} src={groupHero} alt="" />
+        <p className="text-sm text-white">
+          Simply browse through our extensive list of trusted doctors schedule
+          your appointment hassle-free.
+        </p>
+        <button className="flex gap-1 justify-center items-center bg-white rounded-full py-2 px-4">
+          <span>Book appointment </span>
+
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9 18L15 12L9 6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+      <div>
+        <img src={hero} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
