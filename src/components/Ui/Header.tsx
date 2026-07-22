ec2d56b253e8f98358c4ca8d3147105dbd3b132a
elmira1385 <elmirashokriyan64@gmail.com>
2026-07-22 18:38:06 +0430

    somethinges were changhed in header

--- src/components/Ui/Header.tsx
+++ src/components/Ui/Header.tsx
@@ -3,8 +3,11 @@ import icon from "../../images/icon_header.svg";
 
 const Header = () => {
   const [isOpenHeader, setIsOpenHeader] = useState(false);
+  const [active, setActive] = useState<
+    "home" | "allProducts" | "about" | "content"
+  >("home");
   return (
-    <header className="flex py-4 justify-between items-center">
+    <header className="flex py-4 justify-between items-center border-b">
       <div>
         <img width={150} src={icon} alt="icon" />
       </div>
@@ -20,7 +23,6 @@ const Header = () => {
             height="30px"
             viewBox="-3.5 0 19 19"
             xmlns="http://www.w3.org/2000/svg"
-            
           >
             <path d="M11.383 13.644A1.03 1.03 0 0 1 9.928 15.1L6 11.172 2.072 15.1a1.03 1.03 0 1 1-1.455-1.456l3.928-3.928L.617 5.79a1.03 1.03 0 1 1 1.455-1.456L6 8.261l3.928-3.928a1.03 1.03 0 0 1 1.455 1.456L7.455 9.716z" />
           </svg>
@@ -39,13 +41,41 @@ const Header = () => {
       </div>
       {isOpenHeader && (
         <div
-          className={`fixed sm:inline-flex md:inline-flex lg:inline-flex xl:hidden  flex flex-col gap-6 items-center  z-50  bg-[#1b85cc]  w-full h-full top-16  p-4 transition-all duration-300 ${isOpenHeader ? "right-0" : "-right-full"}`}
+          className={`fixed sm:inline-flex md:inline-flex lg:inline-flex xl:hidden  flex flex-col gap-6 items-center  z-50 bg-white  w-full h-full top-15  p-4 transition-all duration-300 ${isOpenHeader ? "right-0" : "-right-full"}`}
         >
-          <ul className="flex flex-col justify-center items-center gap-4  *:text-[16px] *:cursor-pointer">
-           <li>HOME</li>
-           <li>ALL PRODUCTS</li>
-           <li>ABOUT</li>
-           <li>CONTENT</li>
+          <ul className="flex flex-col justify-center items-center gap-6 *:font-bold  *:text-[16px] *:cursor-pointer">
+            <li
+              onClick={() => {
+                setActive("home");
+              }}
+              className={`px-4 py-2 rounded-md ${active === "home" && "bg-primary text-white"}`}
+            >
+              HOME
+            </li>
+            <li
+              onClick={() => {
+                setActive("allProducts");
+              }}
+              className={`px-4 py-2 rounded-md ${active === "allProducts" && "bg-primary text-white"}`}
+            >
+              ALL PRODUCTS
+            </li>
+            <li
+              onClick={() => {
+                setActive("about");
+              }}
+              className={`px-4 py-2 rounded-md ${active === "about" && "bg-primary text-white"}`}
+            >
+              ABOUT
+            </li>
+            <li
+              onClick={() => {
+                setActive("content");
+              }}
+              className={`px-4 py-2 rounded-md ${active === "content" && "bg-primary text-white"}`}
+            >
+              CONTENT
+            </li>
           </ul>
         </div>
       )}
