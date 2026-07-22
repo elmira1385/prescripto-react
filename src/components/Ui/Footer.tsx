aeb30d53180f577043274280ee1ab66c88096cb0
elmira1385 <elmirashokriyan64@gmail.com>
2026-07-23 02:34:02 +0430

    start login

--- src/components/Ui/Footer.tsx
+++ src/components/Ui/Footer.tsx
@@ -3,7 +3,7 @@ import icon from "../../images/icon_header.svg";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 mt-40 text-sm">
+    <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 mt-40 pb-10 text-sm">
       <div className="flex flex-col gap-6">
         <img className=" w-40" src={icon} alt="icon" />
         <p className="w-full md:w-2/3 text-gray-600 leading-6">
@@ -22,7 +22,7 @@ const Footer = () => {
           <li>Privacy policy</li>
         </ul>
       </div>
-      <div className="flex flex-col gap-6 mb-10">
+      <div className="flex flex-col gap-6 ">
         <p className="text-xl font-medium ">GET IN TOUCH</p>
         <ul className="flex flex-col gap-2 text-gray-600">
           <li>+0-000-000-000</li>
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { RouterProvider } from "react-router/dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home.tsx';
 import MainLayout from './components/Layout/MainLayout.tsx';
+import Login from './pages/Login.tsx';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         index:true,
         Component:Home
+      },
+      {
+        path:"/login",
+        Component:Login
       }
     ]
   },
