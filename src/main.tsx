ed4a3aa2d2418872a41a4d8039435965a506b353
elmira1385 <elmirashokriyan64@gmail.com>
2026-07-24 18:13:32 +0430

    login and register are submited

--- src/main.tsx
+++ src/main.tsx
@@ -1,51 +1,53 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Home from './pages/Home.tsx';
-import MainLayout from './components/Layout/MainLayout.tsx';
-import Login from './pages/Login.tsx';
-import AllDoctors from './pages/AllDoctors.tsx';
-import About from './pages/About.tsx';
-import Content from './pages/Content.tsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home.tsx";
+import MainLayout from "./components/Layout/MainLayout.tsx";
+import Login from "./pages/Login.tsx";
+import AllDoctors from "./pages/AllDoctors.tsx";
+import About from "./pages/About.tsx";
+import Content from "./pages/Content.tsx";
+import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component:MainLayout,
-    children:[
+    Component: MainLayout,
+    children: [
       {
-        index:true,
-        Component:Home
+        index: true,
+        Component: Home,
       },
       {
-        path:"/login",
-        Component:Login
+        path: "/login",
+        Component: Login,
       },
       {
-        path:"/AllDoctors",
-        Component:AllDoctors
+        path: "/AllDoctors",
+        Component: AllDoctors,
       },
       {
-        path:"/About",
-        Component:About
+        path: "/About",
+        Component: About,
       },
       {
-        path:"/Content",
-        Component:Content
+        path: "/Content",
+        Component: Content,
       },
-    ]
+    ],
   },
 ]);
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </QueryClientProvider>
   </StrictMode>,
-)
+);
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,106 @@
-import React, { useState } from "react";
-
+import { useMutation } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { api } from "../api/axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
+interface ILoginOrREgister {
+  name?: string;
+  email: string;
+  password: string;
+}
+interface IRegisterOrLoginResponse {
+  success: boolean;
+  token: string;
+}
 const Login = () => {
   const [isRegisterOpen, setIsResterOpen] = useState(true);
+  const route = useNavigate();
+  //signUp
+  const {
+    register: signUpRegister,
+    handleSubmit: signUpHandleSubmit,
+    formState: { errors: errorSignUp },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const {
+    data: singUpData,
+    isSuccess: signUpIsSuccess,
+    isError: signUpIsError,
+    mutate: signUpMutate,
+  } = useMutation({
+    mutationKey: ["register"],
+    mutationFn: async (user: ILoginOrREgister) => {
+      const { data } = await api.post<IRegisterOrLoginResponse>("/api/user/register", {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
+      return data;
+    },
+  });
+  useEffect(() => {
+    if (signUpIsSuccess) {
+      localStorage.setItem("token", singUpData.token);
+      toast.success("you are register please login");
+      setIsResterOpen(false);
+    } else if (signUpIsError) {
+      toast.error("please Try Again");
+    }
+  }, [signUpIsSuccess, signUpIsError]);
+
+  //login
+  const {
+    register: loginRegister,
+    handleSubmit: loginHandleSubmit,
+    formState: { errors: errorLogin },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const {
+    data: loginData,
+    isSuccess: loginIsSuccess,
+    isError: loginIsError,
+    mutate: loginMutate,
+  } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: async (user: ILoginOrREgister) => {
+      const { data } = await api.post<IRegisterOrLoginResponse>("/api/user/login", {
+        email: user.email,
+        password: user.password,
+      });
+      return data;
+    },
+  });
+
+  useEffect(() => {
+    if (loginIsSuccess) {
+      localStorage.setItem("token", loginData.token);
+      route("/");
+      toast.success("you are login");
+    } else if (loginIsError) {
+      toast.error("please check your information");
+    }
+  }, [loginIsSuccess, loginIsError]);
   return (
     <>
       {isRegisterOpen ? (
-        <form className="min-h-[80vh] flex items-center justify-center">
+        <form
+          onSubmit={signUpHandleSubmit(({ name, email, password }) => {
+            signUpMutate({ name, email, password });
+          })}
+          className="min-h-[80vh] flex items-center justify-center"
+        >
           <div className="flex flex-col gap-4  items-start p-12   rounded-xl text-[#5E5E5E] text-sm shadow-lg">
             <div className="flex flex-col gap-2">
               <p className="text-2xl font-semibold">Create Account</p>
@@ -14,42 +109,77 @@ const Login = () => {
             <div className="w-full flex flex-col gap-2 ">
               <p>Full Name</p>
               <input
+                {...signUpRegister("name", {
+                  required: true,
+                })}
                 className="border border-[#DADADA] rounded w-full p-2"
                 type="text"
-                value=""
               />
             </div>
             <div className="w-full flex flex-col gap-2 ">
               <p>Email</p>
               <input
+                {...signUpRegister("email", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                    message: "Please enter a valid email address",
+                  },
+                })}
                 className="border border-[#DADADA] rounded w-full p-2 "
                 type="email"
-                value=""
               />
+              {errorSignUp.email && (
+                <p className="text-red-500 text-xs">
+                  {errorSignUp.email.message}
+                </p>
+              )}
             </div>
             <div className="w-full flex flex-col gap-2 ">
               <p>Password</p>
               <input
+                {...signUpRegister("password", {
+                  required: true,
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                })}
                 className="border border-[#DADADA] rounded w-full p-2 "
                 type="password"
-                value=""
               />
+              {errorSignUp.password && (
+                <p className="text-red-500 text-xs">
+                  {errorSignUp.password.message}
+                </p>
+              )}
             </div>
-            <button className="bg-primary text-white w-full py-2 m rounded-md text-base">
+            <button
+              type="submit"
+              className="bg-primary text-white w-full py-2 m rounded-md text-base"
+            >
               Create account
             </button>
             <p>
               Already have an account?
-              <span onClick={()=>{
-                setIsResterOpen(false)
-              }} className="text-primary underline cursor-pointer">
+              <span
+                onClick={() => {
+                  setIsResterOpen(false);
+                }}
+                className="text-primary underline cursor-pointer"
+              >
                 Login here
               </span>
             </p>
           </div>
         </form>
       ) : (
-        <form className="min-h-[80vh] flex items-center justify-center">
+        <form
+          onSubmit={loginHandleSubmit(({ email, password }) => {
+            loginMutate({ email, password });
+          })}
+          className="min-h-[80vh] flex items-center justify-center"
+        >
           <div className="flex flex-col gap-4  items-start p-12   rounded-xl text-[#5E5E5E] text-sm shadow-lg">
             <div className="flex flex-col gap-2">
               <p className="text-2xl font-semibold">Login</p>
@@ -58,27 +188,55 @@ const Login = () => {
             <div className="w-full flex flex-col gap-2 ">
               <p>Email</p>
               <input
+                {...loginRegister("email", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                    message: "Please enter a valid email address",
+                  },
+                })}
                 className="border border-[#DADADA] rounded w-full p-2 "
                 type="email"
-                value=""
               />
+              {errorLogin.email && (
+                <p className="text-red-500 text-xs">
+                  {errorLogin.email.message}
+                </p>
+              )}
             </div>
             <div className="w-full flex flex-col gap-2 ">
               <p>Password</p>
               <input
+                {...loginRegister("password", {
+                  required: true,
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                })}
                 className="border border-[#DADADA] rounded w-full p-2 "
                 type="password"
-                value=""
               />
+              {errorLogin.password && (
+                <p className="text-red-500 text-xs">
+                  {errorLogin.password.message}
+                </p>
+              )}
             </div>
-            <button className="bg-primary text-white w-full py-2 m rounded-md text-base">
-              Create account
+            <button
+              type="submit"
+              className="bg-primary text-white w-full py-2 m rounded-md text-base"
+            >
+              Login
             </button>
             <p>
               Create a new account?
-              <span  onClick={()=>{
-                setIsResterOpen(true)
-              }}  className="text-primary underline cursor-pointer">
+              <span
+                onClick={() => {
+                  setIsResterOpen(true);
+                }}
+                className="text-primary underline cursor-pointer"
+              >
                 Click here
               </span>
             </p>
